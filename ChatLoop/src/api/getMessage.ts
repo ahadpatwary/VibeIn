a329a35657e7560734d6e611da7a7f82ad087eca
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2025-10-23 02:58:19 +0000

    api handle

---
+++ ChatLoop/src/api/getMessage.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import Message from "../models/Message";
+import { connectToDb } from "../lib/db";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const { userId, chatWith } = req.body;
+
+    await connectToDb();
+
+    const messages = await Message.find({
+      $or: [
+        { sender: userId, receiver: chatWith },
+        { sender: chatWith, receiver: userId },
+      ],
+    }).sort({ createdAt: 1 });
+
+    return res.status(200).json({ messages });
+  } catch (error) {
+    console.error("Error fetching messages:", (error as Error).message);
+    return res.status(500).json({ messages: null });
+  }
+});
+
+export default router;
--- ChatLoop/src/app.ts
+++ ChatLoop/src/app.ts
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 // import messageRoutes from "./api/routes/messageRoutes";
 // import userRoutes from "./api/routes/userRoutes";
+import messageRoutes from './api/getMessage'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/getMessages", messageRoutes);
 
 // app.use("/api/messages", messageRoutes);
 // app.use("/api/users", userRoutes);
