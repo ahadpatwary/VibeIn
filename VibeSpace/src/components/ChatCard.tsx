489d2e8a6320d766b6d37aa17499064931a266ef
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2025-11-03 02:57:59 +0600

    use flex

--- VibeSpace/src/components/ChatCard.tsx
+++ VibeSpace/src/components/ChatCard.tsx
@@ -95,7 +95,7 @@ export default function ChatCard({ userId, chatWith }: { userId: string, chatWit
 
   return (   
         <div className="flex flex-col h-screen w-full border-2 relative">
-          <header className="bg-white p-4 text-gray-700">
+          <header className="bg-white p-4 text-gray-700 flex gap-4 items-center border-b border-gray-300">
             <AvatarDemo src={picture} />
             <h2 className="text-lg font-semibold mt-2">{name}</h2>
             <p className="text-sm text-gray-500">Online</p>
