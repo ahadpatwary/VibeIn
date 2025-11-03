1163531514c5c7e5c602aac988ee6b49b8a0d098
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2025-11-03 21:09:57 +0600

    change logic

--- VibeSpace/src/app/chatloop/page.tsx
+++ VibeSpace/src/app/chatloop/page.tsx
@@ -2,23 +2,24 @@
 'use client';
 import ChatCard from '@/components/ChatCard';
 import { userIdClient } from '@/lib/userId';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function MyChatPage({ searchParams }: { searchParams: { [key: string]: string } }) {
   const userId = searchParams.userId;
   const chatWith = searchParams.chatWith;
-  let storedUserId: string | null = null;
+  const [storedUserId, setStoredUserId] = useState<string | null>(null);
 
   useEffect(() => {
     (async () => {
       try {
-        storedUserId = await userIdClient();
+        const fetchedUserId = await userIdClient();
+        setStoredUserId(fetchedUserId);
       } catch (error) {
         console.error("Error fetching user ID:", error);
       }
     })();
-  }, []);
+  }, [storedUserId]);
 
   if (!storedUserId || storedUserId !== userId) {
     return <div>Please log in to access your chat.</div>;
