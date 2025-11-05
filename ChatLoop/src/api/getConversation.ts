acc0b9d01f41de2eb344c9116872168ba4ec465b
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2025-11-05 16:15:09 +0600

    *

--- ChatLoop/src/api/getConversation.ts
+++ ChatLoop/src/api/getConversation.ts
@@ -11,6 +11,9 @@ router.post("/", async (req, res) => {
 
     if (!userID) return res.status(400).json({ message: "userID missing" });
 
+    const data = await User.findById(new Types.ObjectId(userID));
+    console.log(data);
+
     const conversations = await Conversation.find({
       $or: [
         { senderId: new Types.ObjectId(userID) },
