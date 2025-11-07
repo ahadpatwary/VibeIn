de3da26e8ebd831a165c4f2c20f441af6c177380
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2025-11-07 21:58:06 +0600

    *

--- VibeSpace/src/app/(dashboard)/groupCreate/page.tsx
+++ VibeSpace/src/app/(dashboard)/groupCreate/page.tsx
@@ -20,25 +20,28 @@ function CreateGroup({ disabled = false }: CreatePostProps) {
   } = useCreateGroup();
 
   return (
-    <div className="flex flex-col gap-4 p-4 max-w-[550px] min-w-[250px] w-full border shadow-lg rounded-md m-3">
-      
-      <GroupCreate
-        groupName={groupName}
-        setGroupName={setGroupName}
-        groupBio={groupBio}
-        setGroupBio={setGroupBio}
-        setGroupPicture={setGroupPicture}
-      />
+    <div className='h-dvh w-screen flex justify-center items-center'>
+      <div className="flex flex-col gap-4 p-4 max-w-[550px] min-w-[250px] w-full border shadow-lg rounded-md m-3">
+        
+        <GroupCreate
+          groupName={groupName}
+          setGroupName={setGroupName}
+          groupBio={groupBio}
+          setGroupBio={setGroupBio}
+          setGroupPicture={setGroupPicture}
+        />
 
 
-      <button
-        onClick={handleSubmit}
-        disabled={loading || disabled}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
-      >
-        {loading ? "Creating..." : "Create Post"}
-      </button>
+        <button
+          onClick={handleSubmit}
+          disabled={loading || disabled}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
+        >
+          {loading ? "Creating..." : "Create Post"}
+        </button>
+      </div>
     </div>
+
   );
 }
 
