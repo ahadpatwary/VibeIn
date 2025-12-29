682573b59f448a6966d10e47a1508c712027a9ad
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2025-12-29 00:19:22 +0000

    *

--- Worker/src/lib/redis.ts
+++ Worker/src/lib/redis.ts
@@ -5,11 +5,12 @@ let redisClient: Redis | null = null;
 
 export const connectToRedis = () => {
     try {
-        const redis = new Redis({
-            host: process.env.REDIS_HOST || 'localhost',
-            port: parseInt(process.env.REDIS_PORT || '6379'),
-            password: process.env.REDIS_PASSWORD,
-        });
+        // const redis = new Redis({
+        //     host: process.env.REDIS_HOST || 'localhost',
+        //     port: parseInt(process.env.REDIS_PORT || '6379'),
+        //     password: process.env.REDIS_PASSWORD,
+        // });
+        const redis = new Redis("rediss://default:Ad9AAAIncDEyNGU2OWMzZGM2NDM0YTZkYmEwYmY0ZjA4MGVhMjIzYnAxNTcxNTI@driven-lionfish-57152.upstash.io:6379");
         return redis;
     } catch (error) {
         if(error instanceof Error)
