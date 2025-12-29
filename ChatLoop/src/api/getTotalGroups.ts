f5c6c1200e2bcd03563156c2cff376f8b4a60656
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2025-12-29 03:13:18 +0000

    *

--- ChatLoop/src/api/getTotalGroups.ts
+++ ChatLoop/src/api/getTotalGroups.ts
@@ -1,22 +1,19 @@
-import express, {  Response } from 'express'
+import express, { Request, Response } from 'express';
 import Conversation from '../models/Conversations';
 
-//need userId and groupId
 const router = express.Router();
 
-router.post('/', async( res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
     try {
-    
         const groups = await Conversation.find({ type: 'group' })
             .select('_id extraFields')
             .lean()
             .exec();
 
         return res.status(200).json({ groups });
-
     } catch (error) {
-        return res.status(500).json({ message: 'internal server Error' });
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
-})
+});
 
 export default router;
