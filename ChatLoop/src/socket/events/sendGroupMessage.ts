43b7128600a9d6ac084b27956762a089e06ddf80
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2025-12-31 08:10:29 +0000

    *

--- ChatLoop/src/socket/events/sendGroupMessage.ts
+++ ChatLoop/src/socket/events/sendGroupMessage.ts
@@ -3,19 +3,21 @@ import { Types } from 'mongoose'
 import conversation from '../../models/Conversations';
 import { getRedisClient } from '../../lib/redis';
 
-interface dataType{
-    // type: 'oneToOne'| 'group',
-    messageId: string,
+interface dataType {
+    type: 'oneToOne'| 'group',
+    _id?: string,
+    messageId?: string,
     senderId: string,
     receiverId: string | null,
     name: string,
     picture: string,
-    joinId: string,
+    joinId: string | null,
+    setJoinId:((value: string) => void) | undefined,
     text: string,
     referenceMessage: string | null,
     messageTime: string,
-    conversationName: string | null,
-    conversationPicture: string | null,
+    conversationName: string,
+    conversationPicture: string,
 }
 
 
@@ -25,13 +27,14 @@ export const sendGroupMessageHandler = (io: Server, socket: Socket) => {
         socket.on('sendGroupMessage', async(data: dataType) => {
 
             let {
-                // type, 
+                type, 
                 messageId,
                 senderId,
                 receiverId,
                 name,
                 picture,
                 joinId,
+                setJoinId,
                 text,
                 referenceMessage,
                 messageTime,
@@ -41,6 +44,34 @@ export const sendGroupMessageHandler = (io: Server, socket: Socket) => {
 
             // if(!name || !joinId || !text || !messageTime || !conversationName) return;
 
+
+
+            const Redis = getRedisClient();
+            if(!Redis) return;     
+
+            // joinId has null or string,
+
+            if(joinId === null ){  // first send message
+                try{
+                    const newGroup = await conversation.create({
+                        type: 'oneToOne',
+                        participants: [ new Types.ObjectId(senderId), new Types.ObjectId(receiverId!) ], 
+                        lastMessage: text,
+                        lastMessageTime: new Date(messageTime),
+                    })
+
+                    if(!newGroup) return;
+
+                    joinId = newGroup._id.toString();
+                    socket.join(joinId as string);
+                    if(!!setJoinId) setJoinId(joinId!);
+
+                }catch(err){
+                    console.error('Error creating one-to-one conversation:', err);
+                    return;
+                }
+            }
+        
             let message = {
                 messageId,
                 joinId,
@@ -53,103 +84,38 @@ export const sendGroupMessageHandler = (io: Server, socket: Socket) => {
                 conversationPicture
             };
 
-            const Redis = getRedisClient();
-            if(!Redis) return;     
-         
-            if(senderId && receiverId) {
 
-                const isExistGroup = await conversation.findOne({
-                    type: 'oneToOne',
-                    participants: { $all: [ new Types.ObjectId(senderId), new Types.ObjectId(receiverId) ] }
-                });
+            const key = `chat:list:${joinId}`;
+            await Redis.rpush(key, JSON.stringify(message));
 
+            await Redis.zadd(
+                `user:${senderId}:conversations`,
+                messageTime ? new Date(messageTime).getTime() : Date.now(),
+                joinId,
+            );
 
-                let groupId = '';
-                if(!isExistGroup) {
-                    try{
-                        const newGroup = await conversation.create({
-                            type: 'oneToOne',
-                            participants: [ new Types.ObjectId(senderId), new Types.ObjectId(receiverId) ], 
-                            lastMessage: text,
-                            lastMessageTime: new Date(messageTime),
-                        })
+            await Redis.zadd(
+                `user:${receiverId}:conversations`,
+                messageTime ? new Date(messageTime).getTime() : Date.now(),
+                joinId,
+            );
 
-                        groupId = newGroup._id.toString();
-
-                    }catch(err){
-                        console.error('Error creating one-to-one conversation:', err);
-                        return;
+            await Redis.hset(`conversation:data`,
+                joinId!,
+                JSON.stringify(
+                    {
+                        type: 'oneToOne',
+                        participants: [senderId, receiverId],
+                        extraFields: {
+                            conversationName: conversationName || "",
+                            conversationPicture: conversationPicture || ""
+                        }
                     }
-                } else {
-                    groupId = isExistGroup._id.toString();
-                }
-
-                //message store on list
-                const key = `chat:list:${groupId}`;
-                await Redis.rpush(key, JSON.stringify(message));
-
-                await Redis.zadd(
-                    `user:${senderId}:conversations`,
-                    messageTime ? new Date(messageTime).getTime() : Date.now(),
-                    groupId,
-                );
-
-                await Redis.zadd(
-                    `user:${receiverId}:conversations`,
-                    messageTime ? new Date(messageTime).getTime() : Date.now(),
-                    groupId,
-                );
-
-                await Redis.hset(`conversation:data`,
-                    groupId,
-                    JSON.stringify(
-                        {
-                            type: 'oneToOne',
-                            participants: [senderId, receiverId],
-                            extraFields: {
-                                conversationName: conversationName || "",
-                                conversationPicture: conversationPicture || ""
-                            }
-                        }
-                    )
-                );
-
-                socket.join(groupId);
-                socket.to(groupId).emit('receiveGroupMessage', message)
-                return;
-            }else{
-                const key = `chat:list:${joinId}`;
-                await Redis.rpush(key, JSON.stringify(message));
-
-                await Redis.zadd(
-                    `user:${senderId}:conversations`,
-                    messageTime ? new Date(messageTime).getTime() : Date.now(),
-                    joinId,
-                );
-
-                await Redis.zadd(
-                    `user:${receiverId}:conversations`,
-                    messageTime ? new Date(messageTime).getTime() : Date.now(),
-                    joinId,
-                );
-
-                await Redis.hset(`conversation:data`,
-                    joinId,
-                    JSON.stringify(
-                        {
-                            type: 'oneToOne',
-                            participants: [senderId, receiverId],
-                            extraFields: {
-                                conversationName: conversationName || "",
-                                conversationPicture: conversationPicture || ""
-                            }
-                        }
-                    )
-                );
+                )
+            );
                 
-                socket.to(joinId).emit('receiveGroupMessage', message)
-                return;
-            }
+            socket.to(joinId!).emit('receiveGroupMessage', message)
+            return;
 
         })
     } catch (error) {
