9a5a6494a546d665f97020e383d06870b14dc8d9
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2026-01-06 05:31:52 +0600

    *

--- ChatLoop/src/socket/events/setInterval.ts
+++ ChatLoop/src/socket/events/setInterval.ts
@@ -3,29 +3,39 @@ import { getRedisClient } from "../../lib/redis";
 
 let batchInterval: ReturnType<typeof setInterval> | null = null;
 
+export const initPresenceBatch = async (io: Server) => {
+  const Redis = getRedisClient();
+  if (!Redis) return;
 
-export const initPresenceBatch = (io: Server) => {
-    const Redis = getRedisClient();
-    if (!Redis) return;
+  const LOCK_KEY = "presence:batch:lock";
 
-    // check if interval already running
-    if (!batchInterval) {
-        console.log("interval Start");
-        batchInterval = setInterval(async () => {
-            const userIds = await Redis.smembers("online:users");
+  const lock = await Redis.set(
+    LOCK_KEY,
+    "EX",
+    600 // 10 min
+  );
 
-            const pipeline = Redis.pipeline();
-            userIds.forEach((id: string) =>
-                pipeline.exists(`user:online:${id}`)
-            );
+  if (!lock) {
+    console.log("Another server is running batch job");
+    return;
+  }
 
-            const results = await pipeline.exec();
+  console.log("This server is batch leader");
 
-            const activeUsersList = results && userIds.filter(
-                (_, i) => results[i][1] === 1
-            );
+  batchInterval = setInterval(async () => {
+    const userIds = await Redis.smembers("online:users");
 
-            io.to("allUserOnlineStatues").emit("onlineUser", activeUsersList);
-        }, 5 * 60 * 1000); // 5 min
-    }
+    const pipeline = Redis.pipeline();
+    userIds.forEach(id =>
+      pipeline.exists(`user:online:${id}`)
+    );
+
+    const results = await pipeline.exec();
+
+    const activeUsersList =
+      results && userIds.filter((_, i) => results[i][1] === 1);
+
+    io.to("allUserOnlineStatues").emit("onlineUser", activeUsersList);
+
+  }, 5 * 60 * 1000);
 };
