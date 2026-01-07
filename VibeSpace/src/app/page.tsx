79ae49472b7e9088090bd928293d139c9d678bc0
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2026-01-07 03:31:43 +0000

    *

--- VibeSpace/src/app/page.tsx
+++ VibeSpace/src/app/page.tsx
@@ -1,14 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 
-import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
-import { useRouter } from "next/navigation"
-import Link from "next/link";
-import { addUserId } from "@/lib/features/userId/userIdSlice";
-import { useEffect } from "react";
 
 export default function DarkLandingPageFull() {
-  const router = useRouter()
 
   return (
     <p>Abdul Ahad Patwary Website</p>
