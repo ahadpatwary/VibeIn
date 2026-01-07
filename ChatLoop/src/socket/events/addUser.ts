e0da17b8293a583a6f430d6035199e00b0c4d12d
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2026-01-07 07:35:41 +0600

    *

--- ChatLoop/src/socket/events/addUser.ts
+++ ChatLoop/src/socket/events/addUser.ts
@@ -1,20 +1,20 @@
 import { Server, Socket } from "socket.io";
 import { getRedisClient } from "../../lib/redis";
 
-import { initPresenceBatch } from '../events/setInterval'
+// import { initPresenceBatch } from '../events/setInterval'
 
 
 export const addUserHandler = (io: Server, socket: Socket) => {
 
-    initPresenceBatch(io); 
+    // initPresenceBatch(io); 
     
     socket.on('addUser', async (userId: string) => {
 
         const Redis = getRedisClient();
         if(!Redis) return;
 
-        await Redis.set(`user:online:${userId}`, "1", "EX", 300); // string
-        await Redis.sadd("online:users", userId); //set because set element can't expire
+        // await Redis.set(`user:online:${userId}`, "1", "EX", 300); // string
+        // await Redis.sadd("online:users", userId); //set because set element can't expire
 
         socket.join(`user:${userId}`);
         socket.join('allUserOnlineStatus'); 
--- ChatLoop/src/socket/events/setInterval.ts
+++ ChatLoop/src/socket/events/setInterval.ts
@@ -10,20 +10,20 @@ export const initPresenceBatch = async (io: Server) => {
 
   console.log("This server is batch leader yes");
 
-  batchInterval = setInterval(async () => {
-    const userIds = await Redis.smembers("online:users");
+  // batchInterval = setInterval(async () => {
+  //   const userIds = await Redis.smembers("online:users");
 
-    const pipeline = Redis.pipeline();
-    userIds.forEach(id =>
-      pipeline.exists(`user:online:${id}`)
-    );
+  //   const pipeline = Redis.pipeline();
+  //   userIds.forEach(id =>
+  //     pipeline.exists(`user:online:${id}`)
+  //   );
 
-    const results = await pipeline.exec();
+  //   const results = await pipeline.exec();
 
-    const activeUsersList =
-      results && userIds.filter((_, i) => results[i][1] === 1);
+  //   const activeUsersList =
+  //     results && userIds.filter((_, i) => results[i][1] === 1);
 
-    io.to("allUserOnlineStatues").emit("onlineUser", activeUsersList);
+  //   io.to("allUserOnlineStatues").emit("onlineUser", activeUsersList);
 
-  }, 5 * 60 * 1000);
+  // }, 5 * 60 * 1000);
 };
