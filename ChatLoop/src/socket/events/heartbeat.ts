e95b2da52fef9d60c4679b38a1d736896d1feae0
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2026-01-07 07:37:48 +0600

    *

--- ChatLoop/src/socket/events/heartbeat.ts
+++ ChatLoop/src/socket/events/heartbeat.ts
@@ -4,19 +4,19 @@ import { getRedisClient } from '../../lib/redis';
 
 export const heartbeatHandler = (io: Server, socket: Socket) => {
     
-    socket.on("heartbeat", (userId: string) => {
-        try {
-            const Redis = getRedisClient();
-            if(!Redis){
-                socket.emit('error', "Redis connection missing");
-            }
+    // socket.on("heartbeat", (userId: string) => {
+    //     try {
+    //         const Redis = getRedisClient();
+    //         if(!Redis){
+    //             socket.emit('error', "Redis connection missing");
+    //         }
 
-            Redis.set(`user:${userId}`, "online", "EX", 300); // 5 minutes TTL
+    //         Redis.set(`user:${userId}`, "online", "EX", 300); // 5 minutes TTL
             
-        } catch (error) {
-            if(error instanceof Error) {
-                socket.emit('error', `message: ${error.message}`);
-            }
-        }
-    });
+    //     } catch (error) {
+    //         if(error instanceof Error) {
+    //             socket.emit('error', `message: ${error.message}`);
+    //         }
+    //     }
+    // });
 }
