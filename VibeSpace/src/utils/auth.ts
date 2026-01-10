797d13b832c6e1d6a5aabc899e961f6c29a6792e
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2026-01-10 09:28:05 +0600

    *

--- VibeSpace/src/utils/auth.ts
+++ VibeSpace/src/utils/auth.ts
@@ -105,27 +105,26 @@ export const authOptions: NextAuthOptions = {
         return false;
       }
     },
-    
-      async jwt({ token, user }) {
-        if (user?.id) {
-          token.id = user.id;
-        }
-        return token;
-      },
 
-      async session({ session, token }) {
-        if (token?.id && session.user) {
-          session.user.id = token.id as string;
-        }
-        return session;
-      },
+    async jwt({ token, user }) {
+      if (user?.id) {
+        token.id = user.id;
+      }
+      return token;
     },
 
+    async session({ session, token }) {
+      if (token?.id && session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  
   },
 
   session: {
     strategy: "jwt",
-    maxAge: 15 * 60, // 15 minute
+    maxAge: 15 * 60, // 15 minutes
   },
 
   secret: process.env.NEXTAUTH_SECRET,
