01a2c25848d8ef9ce848192644f0db8a1473dda6
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2026-01-20 16:41:35 +0600

    *

--- VibeSpace/src/app/layout.tsx
+++ VibeSpace/src/app/layout.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from "@/components/TthemeProvider";
 import { Toaster } from "@/components/ui/sonner"
 import StoreProvider from './StoreProvider';
 import AuthProvider from './SessionProvider'
-
+import { headers } from 'next/headers'
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default async function RootLayout({ children }: RootLayoutProps) {
+  const headerList = await headers()
+  const nonce = headerList.get('x-nonce')
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
