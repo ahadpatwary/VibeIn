8c7ef431803bb6842bb888a4eb85579a5fb17201
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2026-01-22 00:01:03 +0000

    *

--- VibeSpace/src/app/(dashboard)/feed/page.tsx
+++ VibeSpace/src/app/(dashboard)/feed/page.tsx
@@ -28,7 +28,7 @@ function Feed() {
       <MenubarDemo />
       <div className='flex-1 min-h-0 max-w-7xl w-full mx-auto px-4 flex justify-around'>
 
-        <Card className='h-[500px] flex-1 min-w-0 mt-2 hidden md:block'>
+        <Card className='flex-1 min-w-0 w-full max-h-auto h-full mt-2 hidden md:block'>
           <UserProfile dot={true}  userId = "" />
         </Card>
 
@@ -61,7 +61,7 @@ function Feed() {
         </div>
           </ScrollArea>
 
-        <Card className='h-[500px] flex-1 min-w-0 mt-2 hidden lg:block'>
+        <Card className='flex-1 min-h-auto h-min min-w-0  mt-2 hidden lg:block'>
           < CarouselDemo />
         </Card>
 
--- VibeSpace/src/app/(dashboard)/notifications/page.tsx
+++ VibeSpace/src/app/(dashboard)/notifications/page.tsx
@@ -4,97 +4,79 @@ import { ShowCard } from '@/components/ShowCard';
 import { CustomWrapper } from '@/components/CustomWrapper'
 import { useSession } from "next-auth/react"
 import { useCards }  from '@/hooks/useFetchCards';
-import { Card, CardContent } from "@/components/ui/card"
+import { Card } from "@/components/ui/card"
 import { UserProfile } from '@/components/UserProfile';
 import CarouselDemo from '@/components/Embla';
-import { useEffect, useState} from 'react';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
+import { MenubarDemo } from '@/components/Bar';
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 function Notification() {
   const { data: session } = useSession()
-
   const { activeCards } = useCards(session?.user.id!);
-  const [hide, setHide] = useState(false);
-  const [profileHide, setProfileHide] = useState(false);
   const router = useRouter();
-
-useEffect(() => {
-  const handleResize = () => {
-    if (window.innerWidth < 1024) {
-      setHide(true);
-    } else {
-      setHide(false);
-    }
-
-    if(window.innerWidth < 768){
-      setProfileHide(true);
-    } else {
-      setProfileHide(false);
-    }
-  };
-
-  // প্রথমবার load হওয়ার সময় check
-  handleResize();
-
-  // resize হলে call হবে
-  window.addEventListener("resize", handleResize);
-
-  // cleanup (খুব important)
-  return () => {
-    window.removeEventListener("resize", handleResize);
-  };
-}, []);
 
   const handleClick = () => {
     router.push('/create-post');
   }
 
-
   return (
+
+    <div className="flex flex-col max-h-screen">
+
+      <MenubarDemo />
+      <div className='flex-1 min-h-0 max-w-7xl w-full mx-auto px-4 flex justify-around'>
+
+        <Card className='h-[500px] flex-1 min-w-0 mt-2 hidden md:block'>
+          <UserProfile dot={true}  userId = "" />
+        </Card>
+
+        <ScrollArea className='flex-2 max-w-[600px] min-w-[310px] w-full flex flex-col mx-2 sm:mx-3 lg:mx-4 overflow-y-auto'>
     
-    <div className='flex max-w-[1280px] justify-around'>
-      <>
-      {
-        !profileHide &&  <Card className='h-[500px] max-w-[35%] flex-1 m-2'>
-        <UserProfile dot={true}  userId = "" />
-      </Card>
-      }
-      </>
-      <div className="flex-2 ">
-        <Card className='h-auto max-w-[600px] min-w-[310px] mx-auto p-0 mt-2'>
-          <Button className="h-13  m-2 w-auto" onClick={handleClick}> Create a post</Button>
-          <div className="p-0 m-0 h-8"></div>
-        </Card>
+        <div className='m-2'>
+          <Card className=' max-w-[600px] min-w-[310px] w-full mx-auto p-0 mt-2'>
+            <Button className="h-13  m-2 w-auto" onClick={handleClick}> Create a post</Button>
+            <div className="p-0 m-0 h-8"></div>
+          </Card>
 
-        <div className='flex-2 max-w-[600px] min-w-[310px] mx-auto'>
-          {
-            activeCards?.map((card) => ( 
-              <ShowCard 
-                key = {card._id} 
-                cardId = {card._id} 
-                userId= {card?.user._id}
-                image = {card.image?.url} 
-                title = {card.title} 
-                description = {card.description} 
-                dot = { (session?.user.id == card?.user._id) } 
-                userName= {card?.user.name}
-                userProfile= {card?.user.picture}
-              />
-            )
-          )} 
+          <div className='flex-1 max-w-[600px] w-full min-w-[310px] mx-auto'>
+            {/* {
+              activeCards?.map((card) => ( 
+                <ShowCard 
+                  key = {card._id} 
+                  cardId = {card._id} 
+                  userId= {card?.user._id}
+                  image = {card.image?.url} 
+                  title = {card.title} 
+                  description = {card.description} 
+                  dot = { (session?.user.id == card?.user._id) } 
+                  userName= {card?.user.name}
+                  userProfile= {card?.user.picture}
+                />
+              )
+            )}  */}
+
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+            <Card className='h-full w-full p-2 my-1 rounded-sm!'>abdul ahad patwary</Card>
+          </div>
+
         </div>
-      </div>
+          </ScrollArea>
 
-
-      <>
-      {
-        !hide && <Card className='h-[500px] max-w-[350px] m-2 flex-1 '>
-        < CarouselDemo />
+        <Card className='h-[500px] flex-1 min-w-0 mt-2 hidden lg:block'>
+          < CarouselDemo />
         </Card>
-      }
-      </>
 
+      </div>
     </div>
 
   );
