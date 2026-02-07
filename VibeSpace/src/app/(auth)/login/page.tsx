140625a4c86c880c8afef0b47a1e76acc9f34435
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2026-02-08 00:33:22 +0600

    *

--- VibeSpace/src/app/(auth)/login/page.tsx
+++ VibeSpace/src/app/(auth)/login/page.tsx
@@ -1,12 +1,12 @@
-"use client";
-
 import { Login } from "@/modules/auth/frontend/components/login";
 
 export default function Home() {
 
   return (
-    <div className = "h-screen flex justify-center items-center"> 
-      <Login />
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-lg flex-col gap-6">
+        <Login />
+      </div>
     </div>
   )
 }
--- VibeSpace/src/app/(auth)/register/page.tsx
+++ VibeSpace/src/app/(auth)/register/page.tsx
@@ -4,7 +4,7 @@ import { Register } from "@/modules/auth/frontend/components/register"
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+      <div className="flex w-full max-w-lg flex-col gap-6">
         <Register />
       </div>
     </div>
