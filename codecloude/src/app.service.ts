a12a2041a4517d79894939ec8b31136bc4eaacb9
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2026-05-08 15:12:07 +0000

    *

--- codecloude/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World! in this application we are going to learn about codecloude and how to use it with nestjs';
-  }
-}
