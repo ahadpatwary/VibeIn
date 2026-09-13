8a2c01dafae0d1c4115e54938f40706347edd810
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2026-09-13 23:19:39 +0600

    workspace change

---
+++ README.md
@@ -0,0 +1,16 @@
+"workspaces": ["apps/*", "packages/*"]
+
+npm i @myorg/redis-client -w apps/services/vibeinbackend
+
+npm install @myorg/redis-client --workspace=apps/services/vibeinbackend
+
+
+// apps/vibeinbackend/package.json
+"dependencies": {
+    "@nestjs/common": "^11.0.1",
+    ...
+    "@myorg/redis-client": "^0.1.0"
+}
+
+
+import CustomRedisClient from '@myorg/redis-client';
--- apps/services/vibeinbackend/src/main.ts
+++ apps/services/vibeinbackend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -9,6 +11,7 @@ async function bootstrap() {
   });
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
 
 // “Even during exams, you won’t need to uninstall VibeIn — because every moment here adds real value to your future.”
